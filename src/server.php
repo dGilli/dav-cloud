79f9d3d9ed3a047d3fb08b468bac2bc0e7148950
dGilli <me@dennisgilli.com>
2026-02-07 10:29:33 +0100

    add logging

--- src/server.php
+++ src/server.php
@@ -5,11 +5,16 @@ use Sabre\DAV;
 // The autoloader
 require __DIR__ . '/../vendor/autoload.php';
 
+$logger = new \Monolog\Logger('SabreDav');
+$logger->pushHandler(new \Monolog\Handler\RotatingFileHandler(__DIR__.'/../data/logs/sabredav.log', 3, \Monolog\Logger::DEBUG, true, 0600));
+
 // Now we're creating a whole bunch of objects
 $rootDirectory = new DAV\FS\Directory(__DIR__ . '/../data/public');
 
 // The server object is responsible for making sense out of the WebDAV protocol
 $server = new DAV\Server($rootDirectory);
+
+$server->setLogger($logger);
 
 // If your server is not on your webroot, make sure the following line has the
 // correct information
